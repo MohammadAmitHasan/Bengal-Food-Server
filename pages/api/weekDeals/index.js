d77a3d0a9bf27b9eeab2559374371117cef1e432
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-06-29 10:54:30 +0600

    Data finalized

--- pages/api/weekDeals/index.js
+++ pages/api/weekDeals/index.js
@@ -4,12 +4,24 @@ const collectins = [
         img: 'https://i.ibb.co/b5ZSTdm/Rectangle-1275-13.png',
         ratting: '5',
         noOfRatting: '121',
-        price: 200,
+        price: 150,
         oldPrice: 300,
         category: 'fruits',
         subCategory: 'Yellow fruits',
         group: 'fresh fruit',
         sku: 'fgf-1'
+    },
+    {
+        name: 'Sauce',
+        img: 'https://i.ibb.co/xjGCM97/Rectangle-1275-14.png',
+        ratting: '5',
+        noOfRatting: '121',
+        price: 200,
+        oldPrice: 300,
+        category: 'Groceries',
+        subCategory: 'red sauces',
+        group: 'sauces',
+        sku: 'fgf-16'
     },
     {
         name: 'Apple',
@@ -24,23 +36,23 @@ const collectins = [
         sku: 'fgf-2'
     },
     {
-        name: 'Product Name',
-        img: 'https://i.ibb.co/G0s6cqD/Rectangle-1275-11.png',
+        name: 'Beef',
+        img: 'https://i.ibb.co/ym2QrSj/Rectangle-1275-4.png',
         ratting: '5',
         noOfRatting: '121',
-        price: 200,
+        price: 700,
         oldPrice: 300,
-        category: 'fruits',
-        subCategory: 'white fruits',
-        group: 'fresh fruit',
-        sku: 'fgf-3'
+        category: 'Meat',
+        subCategory: 'Red Meat',
+        group: 'Beef',
+        sku: 'fgf-10'
     },
     {
         name: 'Lychee',
         img: 'https://i.ibb.co/Cvt8T59/Rectangle-1275-10.png',
         ratting: '5',
         noOfRatting: '121',
-        price: 200,
+        price: 300,
         oldPrice: 300,
         category: 'fruits',
         subCategory: 'white fruits',
@@ -62,9 +74,9 @@ const collectins = [
     {
         name: 'Watermelon',
         img: 'https://i.ibb.co/PNc4Z2T/Rectangle-1275-8.png',
-        ratting: '5',
+        ratting: '3',
         noOfRatting: '121',
-        price: 200,
+        price: 160,
         oldPrice: 300,
         category: 'fruits',
         subCategory: 'green fruits',
@@ -76,7 +88,7 @@ const collectins = [
         img: 'https://i.ibb.co/dpVrV8S/Rectangle-1275-7.png',
         ratting: '5',
         noOfRatting: '121',
-        price: 200,
+        price: 400,
         oldPrice: 300,
         category: 'fruits',
         subCategory: 'red fruits',
@@ -84,29 +96,31 @@ const collectins = [
         sku: 'fgf-7'
     },
     {
-        name: 'Guyava',
-        img: 'https://i.ibb.co/pK1Q89j/Rectangle-1275-6.png',
-        ratting: '5',
-        noOfRatting: '121',
-        price: 200,
-        oldPrice: 300,
-        category: 'fruits',
-        subCategory: 'green fruits',
-        group: 'fresh fruit',
-        sku: 'fgf-8'
-    },
-    {
         name: 'Capsicum',
         img: 'https://i.ibb.co/pwykb11/Rectangle-1275-5.png',
         ratting: '5',
         noOfRatting: '121',
+        price: 100,
+        oldPrice: 300,
+        category: 'vegetables',
+        subCategory: 'green vegetables',
+        group: 'fresh vegetables',
+        sku: 'fgf-9'
+    },
+
+    {
+        name: 'Promegranate',
+        img: 'https://i.ibb.co/PF7rHGT/Rectangle-1275.png',
+        ratting: '5',
+        noOfRatting: '121',
         price: 200,
         oldPrice: 300,
         category: 'fruits',
-        subCategory: 'green fruits',
+        subCategory: 'red fruits',
         group: 'fresh fruit',
-        sku: 'fgf-9'
+        sku: 'fgf-15'
     },
+
 ]
 
 export default function handler(req, res) {
